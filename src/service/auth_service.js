23bc232ce2e62a78fcef29e43c777b9fe84909d6
woodimora <woodimora@gmail.com>
2022-01-17 20:07:59 +0900

    add: add google, github login

--- src/service/auth_service.js
+++ src/service/auth_service.js
@@ -1,9 +1,26 @@
-import firebase from 'firebase';
+import {getAuth, signInWithPopup, GithubAuthProvider, GoogleAuthProvider} from 'firebase/auth';
 
 class AuthService {
+    constructor() {
+        this.firebaseAuth = getAuth();
+        this.googleProvider = new GoogleAuthProvider();
+        this.githubProvider = new GithubAuthProvider();
+    }
+
     login(providerName) {
-        const authProvider = new firebase.auth[`${providerName}AuthProvider`]();
-        return firebase.auth().signInWithPopup(authProvider);
+        const authProvider = this.getProvider(providerName);
+        return signInWithPopup(this.firebaseAuth, authProvider);
+    }
+
+    getProvider(providerName) {
+        switch (providerName) {
+            case 'Google':
+                return this.googleProvider;
+            case 'Github':
+                return this.githubProvider;
+            default:
+                throw new Error(`not supported provider: ${providerName}`);
+        }
     }
 }
 
