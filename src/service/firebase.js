f3aa3f67bf3599a6b9b39fcec59242aa75b8cfb1
woodimora <woodimora@gmail.com>
2022-01-17 17:16:33 +0900

    add: project set up & add firebase

---
+++ src/service/firebase.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_DB_URL,
+};
+
+// Initialize Firebase
+export const firebaseApp = initializeApp(firebaseConfig);
