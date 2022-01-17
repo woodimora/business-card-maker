0b3d80ebf5ed85dfbca52b50e8fa31150e27190e
woodimora <woodimora@gmail.com>
2022-01-17 20:08:30 +0900

    add: add files

---
+++ src/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './app.css';
+import Login from "./components/login/login";
+
+function App({authService}) {
+  return (
+      <Login authService={authService}/>
+  );
+}
+
+export default App;
